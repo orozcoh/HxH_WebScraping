dc973a1c3d8712b6a6531aa66684b5a060b42ad5
Santiago Orozco H <santiago.orozcoh@udea.edu.co>
2022-05-27 01:21:59 -0500

    Creation of directory fixed

---
+++ README.md
@@ -0,0 +1,11 @@
+# Hunter x Hunter manga downloader
+
+## How to use:
+
+-   git clone <>
+-   cd <>
+-   Create virtual environment
+-   pip install -r requirements.txt
+-   source venv/bin/activate
+-   python3 ./src/HxH_WebScraping.py 
+
--- src/HxH_WebScraping.py
+++ src/HxH_WebScraping.py
@@ -1,12 +1,37 @@
+from PIL import Image, ImageDraw, ImageFont
 from bs4 import BeautifulSoup
 import requests
+import sys
 import os
-from PIL import Image, ImageDraw, ImageFont
 import io
 
-if not os.path.exists('Hunter_X'):
-    os.makedirs('Hunter_X')
+#------------------------------- External Variables -----------------------------------
 
+if len(sys.argv) == 2:
+    startChapter =int(sys.argv[1])
+    n = 10
+    print("\nDownload stating from chapter: ", startChapter)
+    print("3rd parameter not given - 10 Chapters to donwload\n")
+elif len(sys.argv) == 3:
+    startChapter = int(sys.argv[1])
+    n = int(sys.argv[2])
+    print("\nDownload stating from chapter: ", startChapter)
+    print("Chapters to download:", n, "\n")
+else:
+    print("\n"," "*3, "-"*15,"\n   Hunter x Hunter\n"," "*3,"-"*15, sep="")
+    startChapter = int(input("Which chapter do you want to start downloading from?  "))-1
+    n = int(input("How many chapters do you want to donwload?  "))
+
+#----------------------------------------------------------------------------------------
+
+#----------------------------------- Directory creation ---------------------------------
+folder_name = "Hunter_X"
+
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
+#----------------------------------------------------------------------------------------
+
+#------------------------------- Download function definition ---------------------------
 def download_chapter(chapter):
   chapter_str = "0"* (2 if len(str(chapter)) == 1 else 1) + str(chapter)
   chapter_pdf_name = "./Hunter_X/HxH_" + chapter_str + ".pdf"
@@ -14,15 +39,11 @@ def download_chapter(chapter):
   font = ImageFont.truetype("./fonts/Road_Rage.otf", 300)
 
   url ="https://hunterxhuntermanga.online/manga/hunter-x-hunter-chapter-" + str(chapter)
-  print("URL:", url)
+  #print("URL:", url)
 
   resp = requests.get(url)
-  #print("\nStatus code:", resp.status_code)
 
   soup = BeautifulSoup(resp.content, 'html.parser')
-
-  #print(soup.prettify())
-  #print("\nManga title:",soup.title.string)
 
   temp_links = soup.find_all("meta", property="og:image")
 
@@ -35,14 +56,11 @@ def download_chapter(chapter):
       if not link in img_links:
           img_links.append(link)
 
-  print("\nChapter", chapter, "has", len(img_links), "pages.\n")
+  print("Downloading",len(img_links), "pages from Chapter:", chapter)
 
   cover = Image.open("./cover/HxH_cover.jpeg")
   draw = ImageDraw.Draw(cover) # Call the ImageDraw functions to make the image editable 
   draw.text((280, 1000), chapter_str, font=font, fill=(0,0,0)) 
-
-  #cover # image with chapter number added
-
 
   chapter_img = []
 
@@ -56,12 +74,10 @@ def download_chapter(chapter):
 
     chapter_img.append(im)
 
-  #print("Saving to PDF")
-
   cover.save(chapter_pdf_name, save_all=True, append_images=chapter_img)
+#----------------------------------------------------------------------------------------
 
-lastChapter = 55
-n = 1
-
-for i in range(lastChapter+1, lastChapter+1+n):
-  download_chapter(i)
+#------------------------------------- MAIN ---------------------------------------------
+if __name__ == "__main__":
+  for i in range(startChapter, startChapter+n):
+    download_chapter(i)
